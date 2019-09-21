891e95ab9830677bcdba1813be80550e83db63e8
Tatiana Klimenko <tk@pale.blue>
2019-09-21 19:42:19 +0300

    Add project

---
+++ Assets/Scripts/MoveTo.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour
+{
+    public Transform goal;
+
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+                goal.position = hit.point;
+
+            if ((agent.destination - goal.position).magnitude < agent.radius)
+                return;
+            
+            agent.destination = goal.position;
+        }
+    }
+}
